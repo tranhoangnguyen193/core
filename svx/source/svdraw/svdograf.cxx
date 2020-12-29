7825329570c82e19454d6cfa728164039d5ab071
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-12-29 10:42:17 +0900

    vcl: remove or rename too long methods names in VectorGraphicData
    Change-Id: I8a64d27927b608afdc7996e0b0bbc59a58fde90f

--- svx/source/svdraw/svdograf.cxx
+++ svx/source/svdraw/svdograf.cxx
@@ -142,7 +142,7 @@ void SdrGrafObj::onGraphicChanged()
     // Skip for PDF as it is only a bitmap primitive in a sequence and
     // doesn't contain metadata. However getting the primitive sequence
     // will also trigger a premature rendering of the PDF.
-    if (rVectorGraphicDataPtr->getVectorGraphicDataType() == VectorGraphicDataType::Pdf)
+    if (rVectorGraphicDataPtr->getType() == VectorGraphicDataType::Pdf)
         return;
 
     const drawinglayer::primitive2d::Primitive2DContainer aSequence(rVectorGraphicDataPtr->getPrimitive2DSequence());
@@ -560,7 +560,7 @@ OUString SdrGrafObj::TakeObjNameSingul() const
 
     if (rVectorGraphicDataPtr)
     {
-        switch (rVectorGraphicDataPtr->getVectorGraphicDataType())
+        switch (rVectorGraphicDataPtr->getType())
         {
         case VectorGraphicDataType::Svg:
         {
@@ -635,7 +635,7 @@ OUString SdrGrafObj::TakeObjNamePlural() const
 
     if (rVectorGraphicDataPtr)
     {
-        switch (rVectorGraphicDataPtr->getVectorGraphicDataType())
+        switch (rVectorGraphicDataPtr->getType())
         {
         case VectorGraphicDataType::Svg:
         {
